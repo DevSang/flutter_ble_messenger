51473df558a47780c1e2cbe3c2f643fb71779c03
izen1231 <izen1231@gmail.com>
2019-12-11 16:43:51 +0900

    Fix: test fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hwa/main.dart';
+import 'package:Hwa/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
