f63eb3467f659cfc71a53e135fd67abd22aaf7be
steve <steve@looncup.com>
2019-12-24 18:05:54 +0900

    cached_image, call_api utility 작업 시작

--- lib/utility/call_api.dart
+++ lib/utility/call_api.dart
@@ -1,7 +1,9 @@
 import 'package:Hwa/constant.dart';
+import 'package:Hwa/utility/eval.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:js';
 import 'package:flutter_test/flutter_test.dart';
 
 /*
@@ -11,23 +13,28 @@ import 'package:flutter_test/flutter_test.dart';
  * @description : Call api utility
  *              - return : response(No Json encode) || Error
  */
-class CallApi {
-  static commonApiCall(Map data, uri) async {
-    var responseBody = null;
-    try {
-      var response = await http.post(Constant.API_SERVER_HTTP + uri, body: data);
 
-      if(response.statusCode == 200) {
-        responseBody = json.encode(response.body);
-        if(responseBody != null) {
-          return response;
-        } else {
-          print("#No response" + json.decode(response.body));
-          return false;
-        }
-      }
-    } catch (e) {
-      expect(e, isUnsupportedError);
-    }
-  }
+class CallApi {
+//  static commonApiCall(Map data, uri, type) async {
+//    var responseBody = null;
+//    try {
+//
+//      var response = await Eval.eval('http.$type($Constant.API_SERVER_HTTP + $uri, body: $data)');
+//
+//      if(response.statusCode == 200) {
+//        responseBody = json.encode(response.body);
+//        if(responseBody != null) {
+//          return response;
+//        } else {
+//          print("#No response" + json.decode(response.body));
+//          return false;
+//        }
+//      }
+//    } catch (e) {
+//      expect(e, isUnsupportedError);
+//    }
+//  }
+//  static testFunction(text){
+//    print(text);
+//  }
 }
