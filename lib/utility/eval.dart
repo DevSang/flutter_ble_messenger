9472cb83087e602a8a2f30693c77fa5bbca71518
steve <steve@looncup.com>
2019-12-24 18:12:26 +0900

    delete eval module

--- lib/utility/eval.dart
+++ lib/utility/eval.dart
@@ -1,16 +1,16 @@
-
-import 'dart:js';
-
-/*
- * @project : HWA - Mobile
- * @author : sh
- * @date : 2019-12-24
- * @description : To use Javascript eval
- */
-
-class Eval {
-  static JsObject eval(String json)
-  {
-    return context.callMethod('eval',['(${json})']);
-  }
-}
+//
+//import 'dart:js';
+//
+///*
+// * @project : HWA - Mobile
+// * @author : sh
+// * @date : 2019-12-24
+// * @description : To use Javascript eval
+// */
+//
+//class Eval {
+//  static JsObject eval(String json)
+//  {
+//    return context.callMethod('eval',['(${json})']);
+//  }
+//}
