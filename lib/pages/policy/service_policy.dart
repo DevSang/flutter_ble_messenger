e1122e99808b487782e5597ea82f9ab252d50656
izen1231 <izen1231@gmail.com>
2020-01-08 14:31:35 +0900

    POLICY

---
+++ lib/pages/policy/service_policy.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ServicePolicyPage extends StatefulWidget {
+  @override
+  _ServicePolicyPageState createState() => _ServicePolicyPageState();
+}
+
+class _ServicePolicyPageState extends State<ServicePolicyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
+
