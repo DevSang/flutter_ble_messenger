2b0ede04d32eb7c306aabb960a1a9ea5d15c0d76
izen1231 <izen1231@gmail.com>
2020-01-09 17:00:31 +0900

    validator username

--- lib/utility/validators.dart
+++ lib/utility/validators.dart
@@ -1,9 +1,16 @@
+import 'package:Hwa/utility/red_toast.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 class Validator{
   String validateUserName(String userName) {
-    if (userName.length >= 10) {
-      return '닉네임은 10자리';
+    if (userName.length >= 7) {
+
+      RedToast.toast("사용 가능한 이름입니다", ToastGravity.TOP);
     }
-    return null;
+    else{
+      RedToast.toast("사용 불가능한 이름입니다", ToastGravity.TOP);
+
+    }
     }
   }
 
